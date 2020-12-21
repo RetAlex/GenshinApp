10c108b57c801ae4db1da3cc51dc35ddbe300bc3
Oleksii Retiznyk <OleksiiRetiznykWork@gmail.com>
2020-12-21 23:42:00 +0200

    Added calc-exp info and half the mobs pictures; Added controller to return the calc-exp info

---
+++ Back/src/main/java/git/retalex/GI/utils/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package git.retalex.GI.utils.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException() {
+        super("The requested resource wasn't found");
+    }
+}
