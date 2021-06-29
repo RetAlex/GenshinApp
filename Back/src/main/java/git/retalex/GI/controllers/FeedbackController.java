258926b082beb92cc3263ee230f55ccce0eae626
Oleksii Retiznyk <OleksiiRetiznykWork@gmail.com>
2021-06-29 19:04:10 +0300

    Added feedback controller docs to swagger

--- Back/src/main/java/git/retalex/GI/controllers/FeedbackController.java
+++ Back/src/main/java/git/retalex/GI/controllers/FeedbackController.java
@@ -3,10 +3,13 @@ package git.retalex.GI.controllers;
 import git.retalex.GI.models.FeedbackPayload;
 import git.retalex.GI.models.FeedbackResponse;
 import git.retalex.GI.services.interfaces.FeedbackService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api("Feedback")
 @RestController
 public class FeedbackController {
     private final FeedbackService feedbackService;
@@ -15,6 +18,7 @@ public class FeedbackController {
         this.feedbackService = feedbackService;
     }
 
+    @ApiOperation(value = "Provide a feedback; On production can be executed once a minute, if limit exceeds - returns false in result", response = FeedbackResponse.class)
     @PostMapping("/sendFeedback")
     public FeedbackResponse provideFeedback(@RequestBody FeedbackPayload payload){
         boolean success = feedbackService.sendFeedback(payload.getContactEmail(), payload.getFeedbackMessage());
