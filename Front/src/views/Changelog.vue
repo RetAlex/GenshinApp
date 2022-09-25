b18878db4d368063fdcfa83c8aa7c631e0a6cd50
RetAlex <oleksiiretiznyk@gmail.com>
2022-09-25 13:22:46 +0200

    Added changelog for 3.0 release

--- Front/src/views/Changelog.vue
+++ Front/src/views/Changelog.vue
@@ -47,7 +47,13 @@ export default {
     return {
       changeLog: [
         {
-          version: "Version 2.10 (Current release)",
+          version: "Version 3.0 (Current release)",
+          changes: ["Added sumeru map", "Added new characters, weapons to filters", "Added markers on sumeru maps", "Minor bugs and improvements"],
+          date: '24 September 2022',
+          link: 'https://github.com/RetAlex/GenshinApp/releases/tag/3.0'
+        },
+        {
+          version: "Version 2.10",
           changes: ["Added mob camps fpr Inazuma", "Updated and improved filters for maps, mainly camp layout filters", "New weapons and characters added to filters", "Bugfixes and stability improvements"],
           date: '23 August 2022',
           link: 'https://github.com/RetAlex/GenshinApp/releases/tag/2.10'
